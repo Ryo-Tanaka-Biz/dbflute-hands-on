087457c707d82747a3f95ea0acb53e3b1cca2c95
ryo.tanaka <ryo.tanaka@bizreach.co.jp>
2025-06-18 19:50:57 +0900

    セクション8：排他制御なしの更新まで

---
+++ src/main/java/org/docksidestage/handson/logic/HandsOn08Logic.java
@@ -0,0 +1,84 @@
+package org.docksidestage.handson.logic;
+
+import javax.annotation.Resource;
+
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.dbflute.exbhv.MemberSecurityBhv;
+import org.docksidestage.handson.dbflute.exbhv.MemberServiceBhv;
+import org.docksidestage.handson.dbflute.exentity.Member;
+import org.docksidestage.handson.dbflute.exentity.MemberSecurity;
+import org.docksidestage.handson.dbflute.exentity.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * author tanaryo
+ */
+public class HandsOn08Logic {
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
+    private static final Logger logger = LoggerFactory.getLogger(HandsOn08Logic.class);
+
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    @Resource
+    private MemberBhv memberBhv;
+    @Resource
+    private MemberSecurityBhv memberSecurityBhv;
+    @Resource
+    private MemberServiceBhv memberServiceBhv;
+    @Resource
+    private CurrentTimeLogic currentTimeLogic;
+
+    // ===================================================================================
+    //                                                                              XXXXXX
+    //                                                                        ============
+
+    /**
+     * 指定された会員を正式会員に更新する
+     * 排他制御ありで更新する
+     * 引数の値で null は許されない
+     *
+     * @param memberId 会員Id
+     * @param versionNo バージョンNo
+     */
+    public void updateMemberChangedToFormalized(Integer memberId, Long versionNo) {
+        assertNotNull(memberId);
+        assertNotNull(versionNo);
+
+        Member member = new Member();
+        member.setMemberId(memberId);
+        member.setMemberStatusCode_正式会員();
+        member.setVersionNo(versionNo);
+        memberBhv.update(member);
+    }
+
+    /**
+     * 指定された会員を正式会員に更新する
+     * 排他制御なしで更新する
+     * 引数の値で null は許されない
+     *
+     * @param memberId 会員Id
+     */
+    public void updateMemberChangedToFormalizedNonstrict(Integer memberId){
+        assertNotNull(memberId);
+
+        Member member = new Member();
+        member.setMemberId(memberId);
+        member.setMemberStatusCode_正式会員();
+        memberBhv.updateNonstrict(member);
+    }
+
+    /**
+     * nullチェック
+     *
+     * @param args 引数
+     */
+    private void assertNotNull(Object args) {
+        if (args == null) {
+            throw new IllegalArgumentException("args is null");
+        }
+    }
+}
--- src/test/java/org/docksidestage/handson/exercise/HandsOn08Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn08Test.java
@@ -1,8 +1,13 @@
 package org.docksidestage.handson.exercise;
 
+import java.util.Optional;
+
 import javax.annotation.Resource;
 
+import org.dbflute.cbean.result.ListResultBean;
 import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.dbflute.exentity.Member;
+import org.docksidestage.handson.logic.HandsOn08Logic;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
 /**
@@ -15,20 +20,110 @@ public class HandsOn08Test extends UnitContainerTestCase {
     @Resource
     private MemberBhv memberBhv;
 
+    @Resource
+    private HandsOn08Logic logic;
+
     // ===================================================================================
-    //                                                                             XXXXXXX
+    //                                                     updateMemberChangedToFormalized
     //                                                                        ============
-    public void test_1() {
+
+    /**
+     * 任意の仮会員の会員IDとバージョンNOを渡して更新すること
+     * テストデータ上で任意の仮会員のIDが何番なのかに依存しないように
+     * 更新処理後、DB上のデータが更新されていることをアサート
+     */
+    public void test_updateMemberChangedToFormalized_会員が更新されていること() {
         // ## Arrange ##
+        Optional<Member> member = findProvisionalMember();
+        Integer memberId = member.map(Member::getMemberId).orElse(null);
+        Long versionNo = member.map(Member::getVersionNo).orElse(null);
+
         // ## Act ##
+        logic.updateMemberChangedToFormalized(memberId, versionNo);
+
         // ## Assert ##
+        member.ifPresent(mb -> {
+            assertTrue(mb.isMemberStatusCode仮会員());
+        });
+
+        memberBhv.selectByPK(memberId).ifPresent(updatedMember -> {
+            assertTrue(updatedMember.isMemberStatusCode正式会員());
+            assertTrue(updatedMember.getVersionNo().equals(versionNo + 1));
+        });
     }
+
+    /**
+     * 何かしらの方法で排他制御例外を発生させてみること
+     * 排他制御例外が発生することをアサート
+     * 排他制御例外の内容をログに出力して目視確認すること
+     */
+    public void test_updateMemberChangedToFormalized_排他制御例外が発生すること() {
+        // ## Arrange ##
+        Optional<Member> member = findProvisionalMember();
+        Integer memberId = member.map(Member::getMemberId).orElse(null);
+        Long versionNo = member.map(Member::getVersionNo).orElse(null);
+
+        // ## Act ##
+        logic.updateMemberChangedToFormalized(memberId, versionNo);
+
+        // ## Assert ##
+        assertException(org.dbflute.exception.EntityAlreadyUpdatedException.class, () -> {
+            logic.updateMemberChangedToFormalized(memberId, versionNo);
+        });
+        // ログ出力も確認した
+        // versionNoを+1すると例外発生しない
+    }
+
     // ===================================================================================
     //                                                                             XXXXXXX
     //                                                                        ============
-    public void test_2() {
+
+    /**
+     * 任意の仮会員の会員IDを渡して更新すること
+     * 更新処理後、DB上のデータが更新されていることをアサート
+     */
+    public void test_updateMemberChangedToFormalizedNonstrict_会員が更新されていること() {
         // ## Arrange ##
+        Optional<Member> member = findProvisionalMember();
+        Integer memberId = member.map(Member::getMemberId).orElse(null);
+
         // ## Act ##
+        logic.updateMemberChangedToFormalizedNonstrict(memberId);
+
         // ## Assert ##
+        member.ifPresent(mb -> {
+            assertTrue(mb.isMemberStatusCode仮会員());
+        });
+
+        memberBhv.selectByPK(memberId).ifPresent(updatedMember -> {
+            assertTrue(updatedMember.isMemberStatusCode正式会員());
+        });
+    }
+
+    /**
+     * 通常なら排他制御例外が起きるはずの状況でも排他制御例外が発生しないことをアサート
+     */
+    public void test_updateMemberChangedToFormalizedNonstrict_排他制御例外が発生しないこと() {
+        // ## Arrange ##
+        Optional<Member> member = findProvisionalMember();
+        Integer memberId = member.map(Member::getMemberId).orElse(null);
+        boolean isThrowException = false;
+
+        // ## Act ##
+        logic.updateMemberChangedToFormalizedNonstrict(memberId);
+        try {
+            logic.updateMemberChangedToFormalizedNonstrict(memberId);
+        } catch (org.dbflute.exception.EntityAlreadyUpdatedException e) {
+            isThrowException = true;
+        }
+
+        // ## Assert ##
+        assertFalse(isThrowException);
+    }
+
+    private Optional<Member> findProvisionalMember() {
+        return memberBhv.selectList(cb -> {
+            cb.query().setMemberStatusCode_Equal_仮会員();
+        }).stream().findFirst();
     }
 }
